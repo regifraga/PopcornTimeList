b64b3b3dacf226fc9e999df466dcbf0fa488d397
regifraga <regifraga@gmail.com>
2019-02-24 04:37:58 -0300

    Adicionado opção de Filtar e Ordenar.

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,9 +1,13 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { IonInfiniteScroll, NavController, LoadingController, IonContent } from '@ionic/angular';
+import { IonInfiniteScroll, NavController, LoadingController, IonContent, ActionSheetController } from '@ionic/angular';
 import { NavigationExtras } from '@angular/router';
+import { ActionSheetButton } from '@ionic/core';
 
 import * as _ from 'underscore';
+
+let genresActionSheetButton: Array<ActionSheetButton> = [];
+let sortByListActionSheetButton: Array<ActionSheetButton> = [];
 
 @Component({
   selector: 'app-list',
@@ -17,24 +21,131 @@ export class ListPage implements OnInit {
   private loading: any;
   private selectedMovie: any;
   public searchTerm: string;
+  public genre: string;
+  public sortBy: string;
   public page = 1;
-  public movies: Array<{ id: number; imdb: string; title: string; imageUrl: string; imageBigUrl: string; description: string; year: number; quality: string; rating: number; genres: string; items: any; ratingColor: string; runtime: string, trailer: string }> = [];
-
+  public movies: Array<{ id: number; imdb: string; title: string; actors: string; imageUrl: string; imageBigUrl: string; description: string; year: number; quality: string; rating: number; genres: string; items: any; ratingColor: string; runtime: string, trailer: string }> = [];
+  private genres: Array<string>;
+  private sortByList: Array<string>;
+  
   constructor(
     private http: HttpClient,
     private navCtrl: NavController,
-    public loadingCtrl: LoadingController
+    public loadingCtrl: LoadingController,
+    private actionSheetCtrl: ActionSheetController
   ) {
+    const self = this;
+    const actionSheetCancelButton = {
+      text: 'Cancel',
+      icon: 'close',
+      role: 'cancel',
+      handler: () => {
+        console.log('Cancel clicked');
+      }
+    };
+    
+    this.sortByList = ['Popularity', 'Date added', 'Year'];
+
+    _.each(this.sortByList, function(sortBy) {
+      sortByListActionSheetButton.push({
+        text: sortBy,
+        icon: undefined,
+        role: undefined,
+        handler: () => {
+          console.log(sortBy);
+          self.searchTerm = '';
+          self.movies = [];
+          self.page = 1;
+          self.sortBy = sortBy;
+          self.loadMovies();
+        }
+      });
+    });
+
+    sortByListActionSheetButton.push(actionSheetCancelButton);
+
+    this.genres = [
+      'All',
+      'Action',
+      'Adventure',
+      'Animation',
+      'Biography',
+      'Comedy',
+      'Crime',
+      'Documentary',
+      'Drama',
+      'Family',
+      'Fantasy',
+      'Film-Noir',
+      'History',
+      'Horror',
+      'Music',
+      'Musical',
+      'Mystery',
+      'Romance',
+      'Sci-Fi',
+      'Short',
+      'Sport',
+      'Thriller',
+      'War',
+      'Western'
+    ];
+
+    _.each(this.genres, function(genre) {
+      genresActionSheetButton.push({
+        text: genre,
+        icon: undefined,
+        role: undefined,
+        handler: () => {
+          console.log(genre);
+          self.searchTerm = '';
+          self.movies = [];
+          self.page = 1;
+          self.genre = genre;
+          self.loadMovies();
+        }
+      });
+    });
+
+    genresActionSheetButton.push(actionSheetCancelButton);
+
+    this.genre = this.genres[0];
+    this.sortBy = this.sortByList[0];
+
     this.loadMovies();
   }
-
 
   public pageScroller(){
     this.pageTop.scrollToTop();
   }
 
+  async showFilters() {
+    const self = this;
+
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Genres: ' + self.genre,
+      buttons: genresActionSheetButton
+    });
+
+    await actionSheet.present();
+  }
+
+  async showSortBy() {
+    const self = this;
+
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Sort By: ' + self.sortBy,
+      buttons: sortByListActionSheetButton
+    });
+
+    await actionSheet.present();
+  }
+
   clearFilter() {
     this.searchTerm = '';
+    this.genre = 'seeds';
+    this.movies = [];
+    this.page = 1;
     this.loadMovies();
   }
 
@@ -44,20 +155,18 @@ export class ListPage implements OnInit {
     }
 
     console.log(this.searchTerm);
-    //console.dir(event);
-
-    //_.debounce(async function() {
-      this.loadMovies(this.searchTerm);
-    //}, 500);
+    this.movies = [];
+    this.page = 1;
+    this.loadMovies();
   }
 
   refresh(event) {
-    this.loadMovies(undefined, function() {
+    this.loadMovies(function() {
       event.target.complete();
     });
   }
 
-  async loadMovies(keywords?, callback?) {
+  async loadMovies(callback?) {
     const loading = await this.loadingCtrl.create({
       spinner: 'crescent',
       message: 'Loading...',
@@ -66,13 +175,15 @@ export class ListPage implements OnInit {
     loading.present();
 
     let self = this;
-    let searchParam = (keywords) ? '&keywords=' + keywords.trim().replace(new RegExp(' ', 'g'), '_') : '';
+    const searchParam = (this.searchTerm) ? '&keywords=' + this.searchTerm.trim().replace(new RegExp(' ', 'g'), '+') : '';
+    const genreParam = (this.genre && this.genre !== this.genres[0]) ? '&genre=' + this.genre.trim() : '';
+    const sortByParam = (this.sortBy && this.sortBy !== this.sortByList[0]) ? '&sort=' + this.sortBy.trim().toLowerCase().replace(new RegExp(' ', 'g'), '') : '&sort=seeds';
 
     if(searchParam) {
       self.page = 1;
     }
 
-    let url = 'http://api.apiumadomain.xyz/list?cb=0.4723313860962022&sort=seeds' + searchParam + '&quality=720p,1080p,4k&app_id=T4P_AND&os=ANDROID&ver=2.8.0&page=' + this.page;
+    let url = 'http://api.apiumadomain.xyz/list?cb=0.4723313860962022' + sortByParam + genreParam + searchParam + '&quality=720p,1080p,4k&app_id=T4P_AND&os=ANDROID&ver=2.8.0&page=' + this.page;
 
     console.log(url);
 
@@ -109,6 +220,7 @@ export class ListPage implements OnInit {
           id: movieList[i].id,
           imdb: movieList[i].imdb,
           title: movieList[i].title,
+          actors: movieList[i].actors,
           imageUrl: movieList[i].poster_med,
           imageBigUrl: movieList[i].poster_big,
           description: movieList[i].description,
@@ -140,7 +252,7 @@ export class ListPage implements OnInit {
     });
 
     loadingDetail.present();
-  const self = this;
+    const self = this;
     this.selectedMovie = movie;
 
     //get subtitles
@@ -186,7 +298,7 @@ export class ListPage implements OnInit {
 
   loadMore(event) {
     setTimeout(() => {
-      this.loadMovies(undefined, function(movies){
+      this.loadMovies(function(movies){
         console.log('Movies loaded: ' + movies.length);
 
         // App logic to determine if all data is loaded
